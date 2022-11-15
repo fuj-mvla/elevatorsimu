849be8c87c27643b9ac4984905072e81b7b7adb3
jack fu <100026991@mvla.net>
2022-11-15 11:52:20 -0800

    gui

--- src/ElevatorSimulation.java
+++ src/ElevatorSimulation.java
@@ -1,8 +1,23 @@
 
 import building.Elevator;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class ElevatorSimulation extends Application {
@@ -14,7 +29,14 @@ public class ElevatorSimulation extends Application {
 	private int passengers;
 	private int time;
 	private Timeline t;
-
+	private Rectangle elevator;
+	private BorderPane bp;
+	private GridPane gp;
+	private Button logging;
+	private int duration = 1000;
+	private int cycleCount = 1;
+	private Label timeLabel;
+	
 	/** Local copies of the states for tracking purposes */
 	private final int STOP = Elevator.STOP;
 	private final int MVTOFLR = Elevator.MVTOFLR;
@@ -35,7 +57,10 @@ public class ElevatorSimulation extends Application {
 		
 	}
 	public void initTimeline() {
+		t = new Timeline(new KeyFrame(Duration.millis(duration),ae -> controller.stepSim()));
+		t.setCycleCount(10);
 		
+	
 	}
 	/**
 	 * Start.
@@ -47,11 +72,44 @@ public class ElevatorSimulation extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// You need to design the GUI. Note that the test name should
 		// appear in the Title of the window!!
+		bp = new BorderPane();
+	
+		// Create the triangles
+			
+	
+	
+		gp = new GridPane();
+		
+		setGridPaneConstraints();
+		Rectangle elevator = new Rectangle(100,100);
+		gp.add(elevator,1,1);
+		bp.setCenter(gp);
+		
+	
+ 	
+		Scene scene = new Scene(bp,800,800);
+		primaryStage.setScene(scene);
 		primaryStage.setTitle("Elevator Simulation - "+ controller.getTestName());
 		primaryStage.show();
 
 		//TODO: Complete your GUI, including adding any helper methods.
 		//      Meet the 30 line limit...
+		
+	}
+	public void setGridPaneConstraints() {
+		for (int i = 0; i < 12; i ++) 
+			gp.getColumnConstraints().add(new ColumnConstraints(50));
+
+		for (int i = 0; i < 12; i ++) 
+			gp.getRowConstraints().add(new RowConstraints(50));
+	}
+	public int getTime() {
+		
+	}
+	public void initializeFloors() {
+		
+	}
+	public void enableLogging() {
 		
 	}
 	
