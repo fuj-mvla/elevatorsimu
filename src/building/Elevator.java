75e41d364c76599406c98fa3b39f6108f1969a23
itai schwarz <100026974@mvla.net>
2022-12-10 22:27:12 -0800

    i think stepsim fixed

--- src/building/Elevator.java
+++ src/building/Elevator.java
@@ -50,7 +50,6 @@ public class Elevator {
 
 	
 	private int passengers;  	// the number of people in the elevator
-	
 	private ArrayList<Passengers>[] passByFloor;  // Passengers to exit on the corresponding floor
 
 	private int moveToFloor;	// When exiting the STOP state, this is the floor to move to without
@@ -86,6 +85,18 @@ public class Elevator {
 		if(this.prevState != this.currState) {
 			timeInState = 0;
 		}
+	}
+	public boolean isDoorClosed() {
+		if(currState == OPENDR) {
+			return false;
+			
+		}
+		return true;
+		
+	}
+	
+	public boolean hasArrivedInNewFloor() {
+		
 	}
 	public int getCapacity() {
 		return capacity;
